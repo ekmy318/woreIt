96b9fd44c0744fa87eaa366b0ebfaa3c6be1f4f7
ekmy318 <eunice.xp@gmail.com>
2019-08-28 17:27:36 -0400

    Home gif

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import './Home.css'
 
 const Home = () => (
   <React.Fragment>
-    <h1>Welcome to Defeat Repeat</h1>
+    <h1>Don&apos;t be an outfit repeater</h1>
+    <img src="https://i.giphy.com/media/QMKxAwSGovO49xCC23/giphy.webp"/>
   </React.Fragment>
 )
 
--- src/components/WebcamCapture/WebcamCapture.js
+++ src/components/WebcamCapture/WebcamCapture.js
@@ -1,76 +1,76 @@
-// import React, { Component } from 'react'
-// import Webcam from 'react-webcam'
+import React, { Component } from 'react'
+import Webcam from 'react-webcam'
 // import PostFormCreate from '../Posts/PostFormCreate'
-//
-// class WebcamCapture extends Component {
-//   state = {
-//     imageData: null,
-//     saveImage: false
-//   }
-//
-//   setRef = webcam => {
-//     this.webcam = webcam
-//   }
-//
-//   capture = () => {
-//     const imageSrc = this.webcam.getScreenshot()
-//     this.setState({
-//       imageData: imageSrc
-//     })
-//   }
-//
-//   onClickRetake = e => {
-//     e.persist()
-//     this.setState({
-//       imageData: null
-//     })
-//   }
-//
-//   onClickSave = e => {
-//     e.persist()
-//     this.setState((previousState) => {
-//       return {
-//         saveImage: !previousState.saveImage
-//       }
-//     })
-//   }
-//
-//   handleChange = e => {
-//     e.persist()
-//     this.setState({
-//       [e.target.name]: e.target.value
-//     })
-//   }
-//
-//   handleSaveSubmit = e => {
-//     e.preventDefault()
-//     const imageObject = {
-//       image_name: this.state.image_name
-//     }
-//     this.props.PostFormCreate(imageObject)
-//   }
-//
-//   render () {
-//     const videoConstraints = {
-//       width: 1280,
-//       height: 720,
-//       facingMode: 'user'
-//     }
-//
-//     return (
-//       <div>
-//         <Webcam
-//           audio={false}
-//           height={350}
-//           ref={this.setRef}
-//           screenshotFormat="image/jpeg"
-//           width={350}
-//           videoConstraints={videoConstraints}
-//         />
-//         <button onClick={this.capture}>Capture photo</button>
-//       </div>
-//     )
-//   }
-// }
-//
-// export default WebcamCapture
+
+class WebcamCapture extends Component {
+  state = {
+    imageData: null,
+    saveImage: false
+  }
+
+  setRef = webcam => {
+    this.webcam = webcam
+  }
+
+  capture = () => {
+    const imageSrc = this.webcam.getScreenshot()
+    this.setState({
+      imageData: imageSrc
+    })
+  }
+
+  onClickRetake = e => {
+    e.persist()
+    this.setState({
+      imageData: null
+    })
+  }
+
+  onClickSave = e => {
+    e.persist()
+    this.setState((previousState) => {
+      return {
+        saveImage: !previousState.saveImage
+      }
+    })
+  }
+
+  handleChange = e => {
+    e.persist()
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSaveSubmit = e => {
+    e.preventDefault()
+    const imageObject = {
+      image_name: this.state.image_name
+    }
+    this.props.PostFormCreate(imageObject)
+  }
+
+  render () {
+    const videoConstraints = {
+      width: 1280,
+      height: 720,
+      facingMode: 'user'
+    }
+
+    return (
+      <div>
+        <Webcam
+          audio={false}
+          height={350}
+          ref={this.setRef}
+          screenshotFormat="image/jpeg"
+          width={350}
+          videoConstraints={videoConstraints}
+        />
+        <button onClick={this.capture}>Capture photo</button>
+      </div>
+    )
+  }
+}
+
+export default WebcamCapture
