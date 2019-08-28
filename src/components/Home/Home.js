50ff4a0a79801721809d02235aaec3fb96fc5767
ekmy318 <eunice.xp@gmail.com>
2019-08-28 17:43:40 -0400

    Gif

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,7 @@ import './Home.css'
 const Home = () => (
   <React.Fragment>
     <h1>Don&apos;t be an outfit repeater</h1>
-    <img src="https://i.giphy.com/media/QMKxAwSGovO49xCC23/giphy.webp"/>
+    <img href="%PUBLIC_URL%/homepage.webp"/>
   </React.Fragment>
 )
 
