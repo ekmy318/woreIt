30a5eb792fffa95fb620e678065bf359a29da87c
ekmy318 <eunice.xp@gmail.com>
2019-08-28 18:12:44 -0400

    Update Gif location

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Image from 'react-image-webp'
 import './Home.css'
 
 const Home = () => (
   <React.Fragment>
     <h1>Don&apos;t be an outfit repeater</h1>
-    <img href="%PUBLIC_URL%/homepage.webp"/>
+    <Image
+      webp={require('./homepage.webp')}
+    />
   </React.Fragment>
 )
 
