80bc0113cf469c149dd8af4694ae335ac7f81180
ekmy318 <eunice.xp@gmail.com>
2019-08-29 22:34:51 -0400

    update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => (
   <React.Fragment>
-    <a href="https://github.com/ekmy318/woreIt" target="_blank" rel="noopener noreferrer">
+    <a href="https://ekmy318.github.io/defeatRepeat/" target="_blank" rel="noopener noreferrer">
       <img src={'../../../../public/gh.jpg'} alt="GH Repo" />
     </a>ekmy318
   </React.Fragment>
